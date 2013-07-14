dfe57f352d8960ff55c5fe8c55df380368d2e843
Andrey Zherebchenko <andy.zherebchenko@gmail.com>
2013-07-14 22:53:35 +0300

    Ticket #7167: rssbus cookbook modified to multiple server instances

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['rssbus']['dir'] = '/usr/lib'
 default['rssbus']['src_tarball'] = 'AS2ConnectorCrossPlatformUnixLinuxJavaSetup.tar.gz'
 default['rssbus']['initd_script'] = 'run.sh.erb'
-default['rssbus']['listening_ports'] = []
+default['rssbus']['listening_ports'] = ['8181','8182']
 default['rssbus']['service'] = 'jetty'
 default['rssbus']['pid_file'] = '/var/run/jetty.pid'
 default['rssbus']['admin_password'] = 'test'
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,56 +9,35 @@
 
 include_recipe "java"
 
+service "jetty" do
+  supports :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+
+execute "create-startup-scripts" do
+  command "update-rc.d -f jetty defaults"
+  not_if {  ::File.exists? "/etc/rc3.d/S20jetty" }
+  notifies :restart, resources(:service => "jetty"), :delayed
+end
+
 template "/etc/init.d/jetty" do
   source "#{node['rssbus']['initd_script']}"
   mode "0755"
   owner "root"
   group "root"
   notifies :run, "execute[create-startup-scripts]", :immediately
-end
-
-execute "create-startup-scripts" do
-  command "update-rc.d -f jetty defaults"
-  not_if {  ::File.exists? "/etc/rc3.d/S20jetty" }
-end
-
-service "jetty" do
-  supports :restart => true, :reload => true
-  action [ :enable, :start ]
+  notifies :restart, resources(:service => "jetty"), :delayed
 end
 
 temp_file = "#{node['rssbus']['dir']}/rssbus_src.tar.gz"
-remote_file "#{temp_file}" do
-    source "https://www.rssbus.com/download/GetFile.aspx?file=free/AAY3-U/setup.tar.gz&name=AS2%20Connector%20(Cross-Platform%20Unix/Linux/Java%20Setup)&go=true"
-    mode "0644"
-    action :create
-    notifies :run, "execute[untar]", :immediately
-    notifies :run, "template[/etc/init.d/jetty]", :immediately
-end
-
-execute "untar" do
-  command "cd #{node['rssbus']['dir']} && tar -xzvf #{temp_file} && rm -f #{temp_file}"
-  not_if {  ::File.exists? "#{node['rssbus']['dir']}/RSSBusApps" }
-  notifies :run, "execute[set-pass]", :immediately
-  notifies :run, "template[#{node['rssbus']['dir']}/RSSBusApps/webserver/etc/jetty.xml]", :immediately
-end
-
-execute "set-pass" do
-  command "cd #{node['rssbus']['dir']}/RSSBusApps && java -jar configure.jar -password #{node['rssbus']['admin_password']} " +
-    "&& touch #{node['rssbus']['dir']}/RSSBusApps/password-is-set"
-  not_if {  ::File.exists? "#{node['rssbus']['dir']}/RSSBusApps/password-is-set" }
-end
-
-template "#{node['rssbus']['dir']}/RSSBusApps/webserver/etc/jetty.xml" do
-  source "jetty.xml.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  variables(
-      :ports => node['rssbus']['listening_ports']
-  )
-  notifies :restart, resources(:service => "jetty"), :immediately
-end
+#remote_file "#{temp_file}" do
+#    source "https://www.rssbus.com/download/GetFile.aspx?file=free/AAY3-U/setup.tar.gz&name=AS2%20Connector%20(Cross-Platform%20Unix/Linux/Java%20Setup)&go=true"
+#    mode "0644"
+#    action :create
+#    notifies :run, "execute[untar]", :immediately
+#    notifies :run, "template[/etc/init.d/jetty]", :immediately
+#end
 
 template "/etc/logrotate.d/jetty" do
   source "jetty.logrotate.erb"
@@ -67,3 +46,101 @@ template "/etc/logrotate.d/jetty" do
   mode 0644
 end
 
+Array(node['rssbus']['listening_ports']).each do | port |
+  execute "untar#{port}" do
+    command "cd #{node['rssbus']['dir']} && tar -xzvf #{temp_file} && mv RSSBusApps RSSBusApps#{port}"
+    not_if {  ::File.exists? "#{node['rssbus']['dir']}/RSSBusApps#{port}" }
+    notifies :run, "execute[set-pass#{port}]", :delayed
+    notifies :create, "template[#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty.xml]", :immediately
+    notifies :create, "template[#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-contexts.xml]", :immediately
+    notifies :create, "template[#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-deploy.xml]", :immediately
+    notifies :create, "template[#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-logging.xml]", :immediately
+    notifies :create, "template[#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-realm.xml]", :immediately
+    notifies :create, "template[#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-requestlog.xml]", :immediately
+    notifies :create, "template[#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-webapps.xml]", :immediately
+  end
+
+  execute "set-pass#{port}" do
+    command "cd #{node['rssbus']['dir']}/RSSBusApps#{port} && java -jar configure.jar -password #{node['rssbus']['admin_password']} " +
+      "&& touch #{node['rssbus']['dir']}/RSSBusApps#{port}/password-is-set"
+    not_if {  ::File.exists? "#{node['rssbus']['dir']}/RSSBusApps#{port}/password-is-set" }
+    action :run
+  end
+
+  template "#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty.xml" do
+    source "jetty.xml.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+        :port => port
+    )
+    notifies :restart, resources(:service => "jetty"), :delayed
+  end
+
+  template "#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-contexts.xml" do
+    source "jetty-contexts.xml.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+        :port => port
+    )
+    notifies :restart, resources(:service => "jetty"), :delayed
+  end
+
+  template "#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-deploy.xml" do
+    source "jetty-deploy.xml.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+        :port => port
+    )
+    notifies :restart, resources(:service => "jetty"), :delayed
+  end
+
+  template "#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-logging.xml" do
+    source "jetty-logging.xml.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+        :port => port
+    )
+    notifies :restart, resources(:service => "jetty"), :delayed
+  end
+
+  template "#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-realm.xml" do
+    source "jetty-realm.xml.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+        :port => port
+    )
+    notifies :restart, resources(:service => "jetty"), :delayed
+  end
+
+  template "#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-requestlog.xml" do
+    source "jetty-requestlog.xml.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+        :port => port
+    )
+    notifies :restart, resources(:service => "jetty"), :delayed
+  end
+
+  template "#{node['rssbus']['dir']}/RSSBusApps#{port}/webserver/etc/jetty-webapps.xml" do
+    source "jetty-webapps.xml.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+        :port => port
+    )
+    notifies :restart, resources(:service => "jetty"), :delayed
+  end
+end # each
